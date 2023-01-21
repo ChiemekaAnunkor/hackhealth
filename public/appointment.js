9a428b694dc70fa3a307dad9fb6475115e083b3f
ChiemekaAnunkor <anunkorjr@gmail.com>
2023-01-21 13:01:58 -0600

    added appoitnemtn

--- public/appointment.js
+++ public/appointment.js
@@ -1,1 +1,25 @@
 const apptButtom = document.querySelector(".apptbutton")
+const timeContainer = document.querySelector(".time-container")
+
+const avaiblitity = ["9:00", "8:00", "10:00"]
+const handleAppointment = (e) => {
+    e.preventDefault()
+
+
+    for (let i = 0; i < avaiblitity.length; i++) {
+
+        console.log(e.srcElement.childNodes)
+        // if (e.target.textContent.trim() == avaiblitity[i]) {
+        //     console.log("works")
+
+        //     const s = document.createElement("p")
+        //     s.innerHTML = avaiblitity[i]
+        //     timeContainer.append(s)
+        // }
+
+    }
+
+
+}
+
+addEventListener("click", handleAppointment)
