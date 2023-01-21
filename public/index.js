d85043e02d603d49808071b4d21d262bfb64f1e4
Asel Williams <aselwilliams22@gmail.com>
2023-01-21 13:47:50 -0600

    adds func in index.js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,17 @@
 const loginBtn = document.querySelector('#login-btn');
 const drContainer = document.querySelector('.doctor-container')
+const logoutBtn = document.querySelector('.logout');
+const deleteBtn=document.querySelector('#delete-btn')
+const prescriptionList=document.querySelector('.prescription-list')
 
-// const url='http://127.0.0.1:5500'
+logoutBtn.addEventListener('click',()=>{
+    sessionStorage.clear();
+})
+
+if(!sessionStorage.getItem('token')){
+    window.location.href='./login.html'
+}
+const url='http://localhost:4000'
 const doctorsArr=[]
 
 const getAllDoctors = () => axios.get('http://localhost:4000/getPhysicians').then(({data: doctors})=> {
@@ -10,6 +20,7 @@ const getAllDoctors = () => axios.get('http://localhost:4000/getPhysicians').the
 
 window.addEventListener("DOMContentLoaded", function () {
     getAllDoctors()
+    getAllPres()
 });
 
 function renderItems(doctors) {
@@ -29,4 +40,38 @@ function renderItems(doctors) {
     </section>`;
     });
     drContainer.innerHTML = doctor;
+  }
+
+  const errCallback = err =>console.log(err)
+  let userId=sessionStorage.getItem('userId')
+  let body=
+  {
+    user_id:userId
+  }
+  
+
+const getAllPres = () => axios.post(`${url}/getPrescriptions`,body).then(({data: presList})=> {
+    console.log(body,'body')
+    renderPres(presList)
+}).catch(errCallback)
+// const createProduct = (id) => axios.post(`${baseURL}/${id}`).then(productCallback).catch(errCallback)
+// const deleteProduct = (id) => axios.delete(`${baseURL}/${id}`).then(productCallback).catch(errCallback)
+
+const renderPres=(presList)=> {
+    let pres=presList.map((el)=> {
+        return `
+        <div class="prescription">
+              <h3 class="pres-title">${el.name}</h3>
+              <h4 class="dosage">${el.dosage} mg</h4>
+              <p>${el.freq} times a day</p>
+              <button id="delete-btn" onclick='deletePres(${el.pres_id})'>X</button>
+            </div>
+      `
+    })
+    prescriptionList.innerHTML = pres;
+}
+
+
+  const deletePres=(id)=> {
+
   }
