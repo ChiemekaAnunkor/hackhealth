e6473b68a6fce1346596f46e8e0b779412c54f25
ChiemekaAnunkor <anunkorjr@gmail.com>
2023-01-21 21:22:51 -0600

    fixed appoint

--- public/appointment.js
+++ public/appointment.js
@@ -1,7 +1,6 @@
 const apptButtom = document.querySelector(".apptbutton")
 const timeContainer = document.querySelector(".time-container")
 const logoutBtn = document.querySelector('.logout');
-
 const doctor = document.getElementById("doctors");
 const date = document.getElementById("date")
 const selectedTime = document.querySelector(".time-container")
@@ -13,10 +12,12 @@ const submitAppointment = document.querySelector(".apt-form")
 let formItem = document.getElementById("timeselector")
 let desc = document.getElementById("description")
 
-if(!sessionStorage.getItem('token')){
-    window.location.href='./login.html'
+let viewAppointment = document.querySelector(".doctor-container")
+
+if (!sessionStorage.getItem('token')) {
+    window.location.href = './login.html'
 }
-logoutBtn.addEventListener('click',()=>{
+logoutBtn.addEventListener('click', () => {
     sessionStorage.clear();
 })
 
@@ -40,7 +41,7 @@ const findDoctor = (e) => {
     let doctor_id = parseInt(doctor.value)
     console.log(doctor_id)
     console.log(date.value)
-    
+
 
     axios.post('http://localhost:4000/getDoctorsAvaiblity', { doctor_id: doctor_id, date: date.value }).then(({ data: doctorsAvaiblity }) => {
         // console.log(doctorsAvaiblity);
@@ -49,28 +50,30 @@ const findDoctor = (e) => {
 
         // console.log(doctorsAvaiblity)
         let doctorTime = avaiblity.map((t, index) => {
-            
-            if (doctorsAvaiblity.length >0){
-                
+
+            if (doctorsAvaiblity.length > 0) {
+
                 for (let i = 0; i < doctorsAvaiblity.length; i++) {
                     // console.log(t == doctorsAvaiblity[i].time);
-                        if (doctorsAvaiblity[i].time != t) {
-                            docstime.push(t)
-    
-                            return `
+                    if (doctorsAvaiblity[i].time != t) {
+                        docstime.push(t)
+
+                        return `
                 <option value="${usTime[index]}">${usTime[index]}</option>
                   
-                ` 
-            }}
-          
+                `
+                    }
                 }
-                else {
-                    docstime.push(t)
-    
-                    return `
+
+            }
+            else {
+                docstime.push(t)
+
+                return `
                     <option value="${usTime[index]}">${usTime[index]}</option>
                 `
-                }})
+            }
+        })
 
         timeContainer.innerHTML = doctorTime
     }).catch((error) => console.log(error))
@@ -89,30 +92,6 @@ const findDoctor = (e) => {
 }
 
 
-// const handleAppointment = (e) => {
-//     e.preventDefault()
-
-//     console.log(selectedTime)
-//     modal.style.display = "block";
-// }
-// span.onclick = function () {
-//     modal.style.display = "none";
-// }
-// window.onclick = function (event) {
-//     event.preventDefault()
-
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-
-//     let availabletimeSelected = docstime.map((d) => {
-//         return `
-//         <option value="${d}">${d}</option>
-//         `
-//     })
-//     availableTimes.innerHTML = availabletimeSelected
-// }
-// selectedTime.addEventListener("click", handleAppointment)
 
 apptButtom.addEventListener("click", findDoctor)
 
@@ -126,20 +105,61 @@ const appointmentSubmit = (e) => {
     let doctor_id = parseInt(doctor.value)
 
     let userId = sessionStorage.getItem('userId')
-    body ={
-        date:date.value,
-        doctor_id:doctor_id,
-        user_id:userId,
-        time:foritemCopy,
-        description:desc.value
+    body = {
+        date: date.value,
+        doctor_id: doctor_id,
+        user_id: userId,
+        time: foritemCopy,
+        description: desc.value
     }
-    console.log(body);
 
-    axios.post('http://localhost:4000/appointment',body).then(
-        ()=>alert("sucesss"))
+    axios.post('http://localhost:4000/appointment', body).then(
+        () => alert("sucesss"))
 }
 
 
-
 submitAppointment.addEventListener("submit", appointmentSubmit)
+
+
+
+
+
+
+let userId = sessionStorage.getItem('userId')
+
+
+
+
+setTimeout(() => {
+    let doctor_id = parseInt(doctor.value)
+
+
+    axios.post('http://localhost:4000/getUserAppointment', { user_id: userId }).then(({ data: dbres }) => {
+        console.log(dbres)
+
+
+        let apointmentdata = dbres.map((apt) => {
+            return `
+      <section class="card">
+      <img
+        src=${apt.img}
+        alt="doctor"
+      />
+      <div class="content">
+      <h3 class="doctor">Dr.${apt.first_name} ${apt.last_name}</h3>
+      <h4 class="title">${apt.title}</h4>
+      <p>Appointment Date : ${apt.date} </p>
+      <p>Appointment Time: ${apt.time}</p>
+      <p>Description: ${apt.description} </p>
+    </div>
+    </section>
+    ` })
+        viewAppointment.innerHTML = apointmentdata
+
+
+    })
+
+
+}, 1000)
+
 
--- public/index.js
+++ public/index.js
@@ -1,35 +1,35 @@
 const loginBtn = document.querySelector('#login-btn');
 const drContainer = document.querySelector('.doctor-container')
 const logoutBtn = document.querySelector('.logout');
-const deleteBtn=document.querySelector('#delete-btn')
-const presName=document.querySelector('#name')
-const dosage=document.querySelector('#dosage')
-const freq= document.querySelector('#freq')
-const presForm= document.querySelector('#pres-form')
-const prescriptionList=document.querySelector('.prescription-list')
+const deleteBtn = document.querySelector('#delete-btn')
+const presName = document.querySelector('#name')
+const dosage = document.querySelector('#dosage')
+const freq = document.querySelector('#freq')
+const presForm = document.querySelector('#pres-form')
+const prescriptionList = document.querySelector('.prescription-list')
 
-logoutBtn.addEventListener('click',()=>{
-    sessionStorage.clear();
+logoutBtn.addEventListener('click', () => {
+  sessionStorage.clear();
 })
 
-if(!sessionStorage.getItem('token')){
-    window.location.href='./login.html'
+if (!sessionStorage.getItem('token')) {
+  window.location.href = './login.html'
 }
-const url='http://localhost:4000'
-const doctorsArr=[]
+const url = 'http://localhost:4000'
+const doctorsArr = []
 
-const getAllDoctors = () => axios.get('http://localhost:4000/getPhysicians').then(({data: doctors})=> {
-    renderItems(doctors)
-}).catch((error)=> console.log(error))
+const getAllDoctors = () => axios.get('http://localhost:4000/getPhysicians').then(({ data: doctors }) => {
+  renderItems(doctors)
+}).catch((error) => console.log(error))
 
 window.addEventListener("DOMContentLoaded", function () {
-    getAllDoctors()
-    getAllPres()
+  getAllDoctors()
+  getAllPres()
 });
 
 function renderItems(doctors) {
-    let doctor = doctors.map((item) => {
-      return `
+  let doctor = doctors.map((item) => {
+    return `
       <section class="card">
       <img
         src=${item.img}
@@ -42,29 +42,29 @@ function renderItems(doctors) {
       <p>End-time: 04:00pm</p>
     </div>
     </section>`;
-    });
-    drContainer.innerHTML = doctor;
-  }
+  });
+  drContainer.innerHTML = doctor;
+}
 
-  const errCallback = err =>console.log(err)
-  let userId=sessionStorage.getItem('userId')
+const errCallback = err => console.log(err)
+let userId = sessionStorage.getItem('userId')
 //   console.log(typeof userId, 'userId')
-  let body=
-  {
-    user_id:+userId
-  }
-  
+let body =
+{
+  user_id: +userId
+}
 
-const getAllPres = () => axios.post(`${url}/getPrescription`,body).then(({data: presList})=> {
-    // console.log(body,'body')
-    renderPres(presList)
+
+const getAllPres = () => axios.post(`${url}/getPrescription`, body).then(({ data: presList }) => {
+  // console.log(body,'body')
+  renderPres(presList)
 }).catch(errCallback)
 
 
 
-const renderPres=(presList)=> {
-    let pres=presList.map((el)=> {
-        return `
+const renderPres = (presList) => {
+  let pres = presList.map((el) => {
+    return `
         <div class="prescription">
               <h3 class="pres-title">${el.name}</h3>
               <h4 class="dosage">${el.dosage} mg</h4>
@@ -72,27 +72,28 @@ const renderPres=(presList)=> {
               <button id="delete-btn" onclick='deletePres(${el.pres_id})'>X</button>
             </div>
       `
-    })
-    prescriptionList.innerHTML = pres;
+  })
+  prescriptionList.innerHTML = pres;
 }
 
 
-  const deletePres=(id)=> {
-     axios.delete(`${url}/deletePrescription/${id}`).then(()=>getAllPres()).catch(errCallback)
-  }
+const deletePres = (id) => {
+  axios.delete(`${url}/deletePrescription/${id}`).then(() => getAllPres()).catch(errCallback)
+}
 
-  const addPrescription =(e)=> {
-    e.preventDefault();
+const addPrescription = (e) => {
+  e.preventDefault();
 
-    let body={
-        name:presName.value,
-        dosage:dosage.value,
-        freq:freq.value,
-        user_id: +userId
-    }
-    axios.post(`${url}/addPrescription`,body).then(()=>getAllPres()).catch(errCallback)
-    presName.value=''
-    dosage.value=''
-    freq.value=''
+  let body = {
+    name: presName.value,
+    dosage: dosage.value,
+    freq: freq.value,
+    user_id: +userId
   }
-  presForm.addEventListener('submit', addPrescription)
+  axios.post(`${url}/addPrescription`, body).then(() => getAllPres()).catch(errCallback)
+  presName.value = ''
+  dosage.value = ''
+  freq.value = ''
+}
+
+presForm.addEventListener('submit', addPrescription)
